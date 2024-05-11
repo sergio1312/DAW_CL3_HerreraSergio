40f8a264a782f556fe865ab8b40265af995622f8
Sergio <serham_95@hotmail.com>
2024-05-10 20:20:51 -0500

    avanve a la mitad

--- src/app/form-llamadas/form-llamadas.component.ts
+++ src/app/form-llamadas/form-llamadas.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-llamadas',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-llamadas.component.html',
   styleUrl: './form-llamadas.component.scss'
 })
 export class FormLlamadasComponent {
+  minutos: number = 0
+  costoTotal: number = 0
 
+  calcularCosto() {
+    if (this.minutos <= 5) {
+      this.costoTotal = 10
+    } else {
+      const minutosAdicionales = this.minutos - 5
+      this.costoTotal = 10 + (minutosAdicionales * 2)
+    }
+  }
 }
